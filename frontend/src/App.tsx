7fb8559557ebcd386479a9c7f09e6c3cc8112518
luiz-matoso <luizhmatoso1340@gmail.com>
2025-10-19 17:03:37 -0300

    feat: add Poppins font and setup React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,26 @@
+import { Routes } from "react-router-dom";
 import "./App.css";
+import { Bounce, ToastContainer } from "react-toastify";
 
 function App() {
-  return <>Vinylla</>;
+  return (
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
+      <Routes></Routes>
+    </>
+  );
 }
 
 export default App;
