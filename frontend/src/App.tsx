1bfcb0e0d0477e583cde7081a86cc5357a02aab6
camila-vieirao <camila.zero70505@gmail.com>
2025-11-08 18:03:24 -0300

    fix: router inside tag main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -49,17 +49,18 @@ function App() {
           />
         </>
       )}
-      <main className={!hideLayout ? "pt-26" : ""}></main>
-      <Routes>
-        <Route path="/" element={<Navigate to="/feed" />} />
-        <Route path="/feed" element={<Feed />} />
-        <Route path="/search" element={<SearchResults />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/artist/:id" element={<ArtistPage />} />
-        <Route path="/album/:id" element={<AlbumPage />} />
-        <Route path="/explore" element={<ExplorePage />} />
-      </Routes>
+      <main className={!hideLayout ? "pt-26" : ""}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/feed" />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/search" element={<SearchResults />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/artist/:id" element={<ArtistPage />} />
+          <Route path="/album/:id" element={<AlbumPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+        </Routes>
+      </main>
       {showPostModal && (
         <CreatePostModal onClose={() => setShowPostModal(false)} />
       )}
