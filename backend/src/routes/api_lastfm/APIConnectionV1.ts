8a81c98e68480535fc8954501aca16dcfc40b0ea
luiz matoso <luizhmatoso1340@gmail.com>
2025-11-09 14:05:50 -0300

    feat: add get top tags

--- backend/src/routes/api_lastfm/APIConnectionV1.ts
+++ backend/src/routes/api_lastfm/APIConnectionV1.ts
@@ -1,10 +1,9 @@
 import express from "express";
-import {
-  getTopAlbumsByTag,
-} from "../../controllers/LastfmController";
+import { getTopAlbumsByTag } from "../../controllers/LastfmController";
 
 const router = express.Router();
 
 router.get("/v1/topalbums/tag/:tag", getTopAlbumsByTag);
+router.get("/v1/toptags", getTopTags);
 
 export default router;
